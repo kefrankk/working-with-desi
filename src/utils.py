09335464a64dc453385533d3befc38a83f6e43c2
kefrankk <ke.heckler95@gmail.com>
2025-09-10 16:45:49 -0300

    feat: add function to get desi spectra using sparcl to utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,31 @@ import matplotlib.pyplot as plt
 from astropy.coordinates import SkyCoord
 from astropy.convolution import convolve, Gaussian1DKernel
 
+# SPARCL imports
+from sparcl.client import SparclClient
+
+# Data Lab imports
+from dl import queryClient as qc
+from dl import authClient as ac
+from getpass import getpass
+
+def get_spec(cons):
+    client = SparclClient()
+    
+    out = ['sparcl_id', 'ra', 'dec', 'redshift', 'spectype', 'objtype', 'data_release', 'desiname', 'zcat_nspec', 'targetid']
+    found_I = client.find(outfields=out, constraints=cons, limit=None)
+    
+    # Define the fields to include in the retrieve function
+    inc = ['sparcl_id', 'specid', 'targetid', 'data_release', 'redshift', 'flux',
+           'wavelength', 'model', 'ivar', 'mask', 'wave_sigma', 'spectype', 'ra', 'dec']
+    
+    ids_I = found_I.ids
+    results_I = client.retrieve(uuid_list=ids_I, include=inc, limit=None, dataset_list = ['DESI-DR1'])
+    
+    results_desi_dr1 = pd.json_normalize(results_I.records)
+
+    return results_desi_dr1
+
 
 def plot_spec(result):
 
