5460cf416c16d99e8897ed13bafd648bca849cc7
kefrankk <ke.heckler95@gmail.com>
2025-09-10 15:00:06 -0300

    feat: add function to match different surveys

--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,43 @@
 
+import pandas as pd
+import astropy.units as u
 import matplotlib.pyplot as plt
+from astropy.coordinates import SkyCoord
 from astropy.convolution import convolve, Gaussian1DKernel
 
 
-def plot_spec(index, results):
-    """
-    Pass an index value and the output from using client.retrieve()
-    to plot the spectrum at the specified index.
-    """
-    
-    record = results.records[index]
+def plot_spec(result):
 
-    sparcl_id = record.sparcl_id
-    data_release = record.data_release
-    flux = record.flux
-    wavelength = record.wavelength
-    model = record.model
-    spectype = record.spectype
-    redshift = record.redshift
-    ra = record.ra
-    dec = record.dec
+    """
+    Plot the spectrum of a single DESI target.
+
+    Parameters
+    ----------
+    result : object
+        A single record from a SPARCL query, containing the spectrum,
+        model, and metadata for the target.
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    This function will overwrite any existing plot.  If you want to
+    create multiple plots, you should use subplots or create a new figure
+    before calling this function.
+
+    """
+
+    sparcl_id       = result.sparcl_id
+    data_release    = result.data_release
+    flux            = result.flux
+    wavelength      = result.wavelength
+    model           = result.model
+    spectype        = result.spectype
+    redshift        = result.redshift
+    ra              = result.ra
+    dec             = result.dec
 
     plt.title(f"Data Set = {data_release}\n"
               f"Type = {spectype}\n"
@@ -39,3 +57,46 @@ def plot_spec(index, results):
     # Overplot the model spectrum in red
     plt.plot(wavelength, model, color='r', label='Model spectrum')
     
+
+def match_galaxies(splus, survey, type='sdss'):
+    # print(splus, survey)
+    """
+    Matches the galaxies between the SPLUS and SDSS datasets.
+
+    Parameters
+    ----------
+    splus : pandas.DataFrame
+        DataFrame containing the SPLUS galaxies.
+    sdss : pandas.DataFrame
+        DataFrame containing the SDSS galaxies.
+
+    Returns
+    -------
+    df_common_filtered : pandas.DataFrame
+        DataFrame with the common galaxies between SPLUS and SDSS.
+    """
+
+    # Convert the RA and DEC columns to astropy SkyCoord objects
+    coords_splus    = SkyCoord(ra=splus["ra"].values * u.degree, dec=splus["dec"].values * u.degree)
+
+    coords     = SkyCoord(ra=survey["ra"].values * u.degree, dec=survey["dec"].values * u.degree)
+    
+    # Tolerance for the match
+    tolerance = 0.5 * u.arcsec
+
+    # Match the galaxies between the two datasets
+    idx, d2d, _ = coords.match_to_catalog_sky(coords_splus)
+
+    # Create a mask for the matches
+    mask = d2d < tolerance
+
+    # Get the matched DataFrame
+    df_matched = splus.iloc[idx].reset_index(drop=True)
+
+    # Concatenate the two DataFrames
+    df_common = pd.concat([survey.reset_index(drop=True), df_matched], axis=1)
+
+    # Filter the DataFrame with the mask
+    df_common_filtered = df_common[mask]
+
+    return df_common_filtered
